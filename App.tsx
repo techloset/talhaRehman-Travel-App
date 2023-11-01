d07d8ce7c49917f50f14c5878a0edcac1682f0d6
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2023-11-01 19:46:46 +0500

    Stack Navigator Done

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screen/home/Home';
+import Booking from './src/screen/booking/Booking';
 
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="Notifications" component={Booking} />
+  </Stack.Navigator>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
