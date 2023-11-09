e78fc1fb8b83ddc60108e2b61e47d2edf5de81bc
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2023-11-09 19:52:07 +0500

    Working on booking page

---
+++ src/components/BookingMenu.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BookingMenu = () => {
+  return (
+    <View>
+      <Text>BookingMenu</Text>
+    </View>
+  )
+}
+
+export default BookingMenu
+
+const styles = StyleSheet.create({})
--- src/screen/booking/Booking.jsx
+++ src/screen/booking/Booking.jsx
@@ -1,14 +1,112 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView, Image } from 'react-native'
 import React from 'react'
+import BookingMain from "../../assets/images/BookingMain.png"
+import LeftIcon from "../../assets/images/leftIcon.png"
+import ShareIcon from "../../assets/images/shareIcon.png"
+import HeartIcon from "../../assets/images/heartIcon.png"
+import ratio from '../../style/ratio'
+import StarIcon from "../../assets/images/starIcon.png"
+import BookingMenu from '../../components/BookingMenu'
 
+const { widthPixel, fontPixel, pixelSizeVertical } = ratio;
 const Booking = () => {
   return (
-    <View>
-      <Text>Booking</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <View style={styles.container}>
+          <Image source={BookingMain} style={styles.mainImg} />
+          <View style={styles.header}>
+            <Image source={LeftIcon}/>
+            <View style={styles.share}>
+              <Image source={ShareIcon}/>
+              <Image source={HeartIcon}/>
+            </View>
+          </View>
+          <View style={styles.bali}>
+            <Text style={styles.baliHeading}>BALI</Text>
+            <Text style={styles.indonesia }>Indonesia</Text>
+            <View style={styles.rating}>
+              <Text style={styles.ratingNumber}>
+                4.9
+              </Text>
+              <Image source={StarIcon}/>
+            </View>
+          </View>
+        </View>
+        <View style={styles.downContainer}>
+          <BookingMenu/>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Booking
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container:{
+    // flex:1,
+  },
+  mainImg:{
+    position:"absolute",
+    width:"100%",
+  },
+  header:{
+    display:"flex",
+    flexDirection:'row',
+    justifyContent:"space-between",
+    marginHorizontal:pixelSizeVertical(16),
+    marginTop:pixelSizeVertical(38)
+
+  },
+  share:{
+    display:"flex",
+    flexDirection:"row",
+    gap:pixelSizeVertical(20)
+  },
+  bali:{
+    display:"flex",
+    flexDirection:"column",
+    marginTop:pixelSizeVertical(37),
+    alignItems:"center",
+    gap:pixelSizeVertical(8)
+    // alignContent:'center',
+  },
+  baliHeading:{
+    fontSize:fontPixel(34),
+    fontFamily:"Poppins-Regular",
+    color:"rgba(255, 251, 254, 1)"
+  },
+  indonesia:{
+    fontSize:fontPixel(12),
+    fontFamily:"Poppins-Regular",
+    color:"rgba(255, 251, 254, 1)",
+
+  },
+  rating:{
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"center",
+    alignContent:'center',
+    alignContent:"center",
+    gap:pixelSizeVertical(5),
+    paddingHorizontal:pixelSizeVertical(20),
+    paddingVertical:pixelSizeVertical(5),
+    borderRadius:25,
+    backgroundColor:"#fff"
+  },
+  ratingNumber:{
+    fontSize:fontPixel(12),
+    fontFamily:"Poppins-SemiBold",
+    color:"rgba(0, 0, 0, 1)"
+  },
+  downContainer:{
+    borderRadius:30,
+    backgroundColor:"#F2F5FA",
+    paddingTop:pixelSizeVertical(10),
+    paddingBottom:pixelSizeVertical(20),
+    height:"50%",
+    marginTop:pixelSizeVertical(60)
+  }
+})
